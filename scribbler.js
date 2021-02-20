ebe76d36394339f6939c91986467702635ed105e
Vedobrato Chatterjee <vedochat@gmail.com>
2021-02-20 23:00:11 +0000

    Change

--- scribbler.js
+++ scribbler.js
@@ -21,6 +21,7 @@ if (document.getElementsByClassName('demo').length > 0) {
             - recent interests
             - project portfolio
             - my philosophy`;
+  var result = str.fontsize(7);
   var speed = 60;
 
   function typeItOut () {
